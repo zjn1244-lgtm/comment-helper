4a65f318ab60f100efe4cb7cb65dfb4346ad9e86
zjn <zjn1244@gmail.com>
2026-06-10 13:50:32 +0800

    完成Cookie工作区持久化

--- core/utils.py
+++ core/utils.py
@@ -1,27 +1,40 @@
+import os
 import uuid
 
 import streamlit as st
+from streamlit_cookies_manager import EncryptedCookieManager
+
+
+COOKIE_PREFIX = "comment_ops_helper/"
+COOKIE_PASSWORD = os.environ.get(
+    "COOKIE_PASSWORD",
+    "comment-ops-helper-workspace-cookie",
+)
+WORKSPACE_COOKIE_KEY = "workspace_id"
+
+
+def get_cookie_manager():
+    cookies = EncryptedCookieManager(
+        prefix=COOKIE_PREFIX,
+        password=COOKIE_PASSWORD,
+    )
+
+    if not cookies.ready():
+        st.stop()
+
+    return cookies
 
 
 def get_workspace_id():
-    workspace_id = st.query_params.get("workspace_id")
+    cookies = get_cookie_manager()
+    workspace_id = cookies.get(WORKSPACE_COOKIE_KEY)
 
-    if isinstance(workspace_id, list):
-        workspace_id = workspace_id[0] if workspace_id else None
+    if not workspace_id:
+        workspace_id = str(uuid.uuid4())
+        cookies[WORKSPACE_COOKIE_KEY] = workspace_id
+        cookies.save()
 
-    if workspace_id:
+    if st.session_state.get("workspace_id") != workspace_id:
         st.session_state["workspace_id"] = workspace_id
-        return workspace_id
-
-    workspace_id = st.session_state.get("workspace_id")
-
-    if workspace_id:
-        st.query_params["workspace_id"] = workspace_id
-        return workspace_id
-
-    workspace_id = str(uuid.uuid4())
-    st.session_state["workspace_id"] = workspace_id
-    st.query_params["workspace_id"] = workspace_id
-    st.rerun()
 
     return workspace_id
